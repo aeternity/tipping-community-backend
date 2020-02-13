f9f6c21ae76ad98bb7a416003db0f78bc21a77f5
keno dressel <aeternity@keno.digital>
2020-02-13 17:25:02 +0100

    adds domLoader

---
+++ utils/domLoader.js
@@ -0,0 +1,31 @@
+// load dom from url
+const puppeteer = require('puppeteer');
+
+module.exports = class DomLoader {
+
+  static async getHTMLfromURL(url) {
+    const browser = await puppeteer.launch(process.env.NODE_ENV === 'test' ? {} : {
+      executablePath: '/usr/bin/chromium-browser',
+      args: ['--disable-dev-shm-usage'],
+    });
+    try {
+      const page = await browser.newPage();
+      await page.goto(url, {
+        waitUntil: 'networkidle0',
+      });
+      if (
+        (new URL(url)).hostname === 'www.weibo.com' &&
+        (new URL(page.url())).hostname === 'passport.weibo.com'
+      ) {
+        await page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 40000 });
+      }
+
+      const html = await page.content();
+      await browser.close();
+      return html;
+    } catch (e) {
+      await browser.close();
+      return null;
+    }
+  }
+};

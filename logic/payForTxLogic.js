2417358407cf1d522dd349cce155e3ef0047bd2a
keno dressel <aeternity@keno.digital>
2020-02-19 13:08:01 +0100

    adds getAddresses endpoint

--- logic/payForTxLogic.js
+++ logic/payForTxLogic.js
@@ -15,16 +15,37 @@ module.exports = class PayForTxLogic {
     try {
       const { html } = await DomLoader.getHTMLfromURL(req.body.url);
       if (!html) return PayForTxLogic.sendError(req, res, 500, 'could not retrieve html for url');
-      let address = html.match(/(ak\_[A-Za-z0-9]{49,50})/g) || [];
+      let addresses = html.match(/(ak\_[A-Za-z0-9]{49,50})/g) || [];
       const chainNames = html.match(/[A-Za-z0-9]+\.chain/g);
-      if (chainNames) address = [...address, ...(await ae.getAddressFromChainName(chainNames))];
-      if (address.length > 0) {
-        if (!address.includes(req.body.address)) return PayForTxLogic.sendError(req, res, 401, 'found address does not match requested address');
+      if (chainNames) addresses = [...addresses, ...(await ae.getAddressFromChainName(chainNames))];
+      if (addresses.length > 0) {
+        if (!addresses.includes(req.body.address)) return PayForTxLogic.sendError(req, res, 401, 'found address does not match requested address');
         const result = await ae.claimTips(req.body.address, req.body.url);
         // TODO save tx hash to log
         return PayForTxLogic.sendSuccess(req, res);
       } else {
-        return PayForTxLogic.sendError(req, res, 401, 'Could not find address in website');
+        return PayForTxLogic.sendError(req, res, 401, 'Could not find any address in website');
+      }
+    } catch (e) {
+      return PayForTxLogic.sendError(req, res, 500, e.message);
+    }
+  }
+
+  static async getAddressesFromPage (req, res) {
+    if (!req.body) return PayForTxLogic.sendError(req, res, 400, 'no request body found');
+    if (!req.body.url) return PayForTxLogic.sendError(req, res, 400, 'url not found in body');
+    if (!ae || !ae.client) return PayForTxLogic.sendError(req, res, 500, 'sdk not initialized yet');
+
+    try {
+      const { html } = await DomLoader.getHTMLfromURL(req.body.url);
+      if (!html) return PayForTxLogic.sendError(req, res, 500, 'could not retrieve html for url');
+      let addresses = html.match(/(ak\_[A-Za-z0-9]{49,50})/g) || [];
+      const chainNames = html.match(/[A-Za-z0-9]+\.chain/g);
+      if (chainNames) addresses = [...addresses, ...(await ae.getAddressFromChainName(chainNames))];
+      if (addresses.length > 0) {
+        return res.send({ addresses });
+      } else {
+        return PayForTxLogic.sendError(req, res, 401, 'Could not find any addresses in website');
       }
     } catch (e) {
       return PayForTxLogic.sendError(req, res, 500, e.message);

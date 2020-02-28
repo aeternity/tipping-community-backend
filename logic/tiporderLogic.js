d7b90fc725029a9fa21345d123660b59a0721992
Philipp Piwo <github@piwo.space>
2020-02-28 22:07:51 +0100

    fixed scoring with new contract

--- logic/tiporderLogic.js
+++ logic/tiporderLogic.js
@@ -1,6 +1,6 @@
 const BigNumber = require('bignumber.js');
 
-const ae = require('../utils/aeternity.js');
+const aeternity = require('../utils/aeternity.js');
 const {BlacklistEntry} = require('../utils/database.js');
 const dateAgeScoreWeight = 1.5;
 const tipAmountScoreWeight = 1;
@@ -14,44 +14,38 @@ module.exports = class Tiporder {
     const blacklist = await BlacklistEntry.findAll({raw: true});
     const blacklistedIds = blacklist.map(b => b.tipId);
 
-    // TODO rewrite
-    const state = await ae.getTips();
+    const state = await aeternity.getTips();
 
-    const maxTipAmount = BigNumber.max(...state.map(([_, data]) => data.amount), '1');
-    const tips = state.map(([tip, data]) => {
-      // compute tip id
-      data.tipId = tip.join(',');
-
-      // scoring
-
+    const maxTipAmount = BigNumber.max(...state.map(tip => tip.total_amount), '1');
+    const tips = state.map(tip => {
       //remove some dates that are older than .9995%
-      const datesToConsiderScore = Math.max(((data.received_at / new Date().getTime()) - 0.9995) * 1000, 0);
+      const datesToConsiderScore = Math.max(((tip.timestamp / new Date().getTime()) - 0.9995) * 1000, 0);
       //decay older dates more than newer ones
       const dateAgeScore = datesToConsiderScore === 0 ? 0 : Math.max(1 + Math.log10(datesToConsiderScore), 0);
       //score if title is set
-      const tipTitleScore = data.note ? 1 : 0;
+      const tipTitleScore = tip.note ? 1 : 0;
       //order tutorial highest
-      const featuredScore = tip[0] === "https://medium.com/@coronanewsorg/corona-wallet-beginners-guide-a46e2f845832" ? 1 : 0;
+      const featuredScore = tip.url === "https://medium.com/@coronanewsorg/corona-wallet-beginners-guide-a46e2f845832" ? 1 : 0;
       //score tip amount by percentage of highest amount, no decay
-      const tipAmountScore = new BigNumber(data.amount).dividedBy(maxTipAmount).toNumber();
+      const tipAmountScore = new BigNumber(tip.amount).dividedBy(maxTipAmount).toNumber();
 
-      data.dateAgeScore = dateAgeScore;
-      data.tipAmountScore = tipAmountScore;
-      data.tipTitleScore = tipTitleScore;
-      data.featuredScore = featuredScore;
-      data.score = data.dateAgeScore * dateAgeScoreWeight +
-        data.tipAmountScore * tipAmountScoreWeight +
-        data.tipTitleScore * tipTitleScoreWeight +
-        data.featuredScore * featuredScoreWeight;
+      tip.dateAgeScore = dateAgeScore;
+      tip.tipAmountScore = tipAmountScore;
+      tip.tipTitleScore = tipTitleScore;
+      tip.featuredScore = featuredScore;
+      tip.score = tip.dateAgeScore * dateAgeScoreWeight +
+        tip.tipAmountScore * tipAmountScoreWeight +
+        tip.tipTitleScore * tipTitleScoreWeight +
+        tip.featuredScore * featuredScoreWeight;
 
-      return data;
+      return tip;
     });
 
     const blacklistFiltered = tips
-      .filter(data => !blacklistedIds.includes(data.tipId))
+      .filter(tip => !blacklistedIds.includes(tip.id))
       .map(tip => {
         return {
-          id: tip.tipId,
+          id: tip.id,
           score: tip.score
         }
       });

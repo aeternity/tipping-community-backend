d56948e4ef50d6ab5c964fc0da5690dd7fe8d550
keno dressel <aeternity@keno.digital>
2020-03-31 13:12:25 +0200

    adds sample test with stubbing

--- test/verified.js
+++ test/verified.js
@@ -3,23 +3,33 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
+const ae = require('../utils/aeternity.js');
+const sinon = require('sinon');
 
 chai.use(chaiHttp);
 //Our parent block
 describe('Verified', () => {
-  before(async function () {
-    this.timeout(25000);
-    const ae = require('../utils/aeternity.js');
-    await ae.init();
-  });
-
   describe('Verified API', () => {
     it('it should GET all the verified entries', (done) => {
+      const stub = sinon.stub(ae, 'getTips').callsFake(function () {
+        return [
+          {
+            url: 'https://www.test.domain.com',
+            claim: {
+              unclaimed: false,
+            }
+          },
+        ];
+      });
       chai.request(server).get('/verified/').end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('array');
+        res.body.should.have.length(1);
+        res.body[0].should.equal('https://www.test.domain.com');
+        stub.called.should.be.true;
+        stub.restore();
         done();
       });
-    }).timeout(10000);
+    });
   });
 });

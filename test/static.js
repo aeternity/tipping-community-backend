67ba1a1c462988d42c17362383af8f7d13133fd5
keno dressel <aeternity@keno.digital>
2020-04-28 13:14:20 +0200

    adds hardcoded url graylist

--- test/static.js
+++ test/static.js
@@ -66,4 +66,14 @@ describe('Static Routes', () => {
       });
     });
   });
+  describe('GrayList', () => {
+    it('it should GET the hardcoded graylist', (done) => {
+      chai.request(server).get('/static/wallet/graylist').end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        res.body.includes('facebook.com').should.be.true;
+        done();
+      });
+    });
+  });
 });

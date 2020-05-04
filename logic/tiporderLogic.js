0f0a3942fccf6285f5317d48ed191fa2e0683ab7
Philipp Piwo <github@piwo.space>
2020-05-04 17:19:42 +0200

    fix broken scoring (#105)
    * fix broken scoring
    * commented out featured tip scoring

--- logic/tiporderLogic.js
+++ logic/tiporderLogic.js
@@ -5,7 +5,7 @@ const {BlacklistEntry} = require('../models');
 const dateAgeScoreWeight = 1.5;
 const tipAmountScoreWeight = 1;
 const tipTitleScoreWeight = 0.7;
-const featuredScoreWeight = 10;
+//const featuredScoreWeight = 10;
 
 module.exports = class Tiporder {
 
@@ -24,18 +24,18 @@ module.exports = class Tiporder {
       //score if title is set
       const tipTitleScore = tip.note ? 1 : 0;
       //order tutorial highest
-      const featuredScore = tip.url === "https://medium.com/@coronanewsorg/corona-wallet-beginners-guide-a46e2f845832" ? 1 : 0;
+      //const featuredScore = tip.url === "https://medium.com/@coronanewsorg/corona-wallet-beginners-guide-a46e2f845832" ? 1 : 0;
       //score tip amount by percentage of highest amount, no decay
-      const tipAmountScore = new BigNumber(tip.amount).dividedBy(maxTipAmount).toNumber();
+      const tipAmountScore = new BigNumber(tip.amount_ae).dividedBy(maxTipAmount).toNumber();
 
       tip.dateAgeScore = dateAgeScore;
       tip.tipAmountScore = tipAmountScore;
       tip.tipTitleScore = tipTitleScore;
-      tip.featuredScore = featuredScore;
+      //tip.featuredScore = featuredScore;
       tip.score = tip.dateAgeScore * dateAgeScoreWeight +
         tip.tipAmountScore * tipAmountScoreWeight +
-        tip.tipTitleScore * tipTitleScoreWeight +
-        tip.featuredScore * featuredScoreWeight;
+        tip.tipTitleScore * tipTitleScoreWeight
+        //tip.featuredScore * featuredScoreWeight;
 
       return tip;
     });

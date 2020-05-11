cc12766f3f25cef6269e293b482c5a5e4e1b4c44
keno dressel <aeternity@keno.digital>
2020-05-11 11:13:05 +0200

    adds some more tip tracing

--- logic/tracingLogic.js
+++ logic/tracingLogic.js
@@ -2,28 +2,40 @@ const { v4: uuidv4 } = require('uuid');
 
 module.exports = class TracingLogic {
 
-  constructor() {
+  constructor () {
     this.id = uuidv4();
     // TODO create new entry
-    this.data = []
+    this.data = [];
   }
 
   update (update) {
-    this.data.push(Object.assign(update, {date: new Date()}))
+    this.data.push(Object.assign(update, { date: new Date() }));
+  }
+
+  catchError (returnValue) {
+    return (e) => {
+      this.update({ state: TracingLogic.state.CAUGHT_ERROR, error: Object.assign({}, e) });
+      return returnValue
+    }
   }
 
   finished (result) {
-    this.update(Object.assign({state: TracingLogic.state.FINISHED}, result))
-    console.log("TRACE", this.id)
-    console.log(this.data)
+    this.update(Object.assign({ state: TracingLogic.state.FINISHED }, result));
+    console.log('TRACE', this.id);
+    console.log(this.data);
   }
 
   static state = {
     REQUEST_RECEIVED: 'REQUEST_RECEIVED',
     BODY_RECEIVED: 'BODY_RECEIVED',
     DATA_PARSED: 'DATA_PARSED',
+    CLAIM_AMOUNT: 'CLAIM_AMOUNT',
+    INITIAL_PRECLAIM_RESULT: 'INITIAL_PRECLAIM_RESULT',
+    ESTIMATED_FEE: 'ESTIMATED_FEE',
+    PRECLAIM_STARTED: 'PRECLAIM_STARTED',
     STARTED_PRE_CLAIM: 'STARTED_PRE_CLAIM',
+    CAUGHT_ERROR: 'CAUGHT_ERROR',
     ERROR: 'ERROR',
-    FINISHED: 'FINISHED'
+    FINISHED: 'FINISHED',
   };
 };

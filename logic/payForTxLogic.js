49e401ea4b0f21f1ab09a4ad5347b07f88bbee23
keno dressel <aeternity@keno.digital>
2020-05-13 18:03:49 +0200

    adds basic routes & migration

--- logic/payForTxLogic.js
+++ logic/payForTxLogic.js
@@ -12,44 +12,44 @@ module.exports = class PayForTxLogic {
     // Create new trace for each claim
     const trace = new Trace();
     trace.update({
-      state: Trace.state.REQUEST_RECEIVED
-    })
+      state: Trace.state.REQUEST_RECEIVED,
+    });
 
     // Helper functions
     const sendSuccess = () => {
       logger.log({ success: true, url: req.body.url, address: req.body.address, status: 200, message: '' });
       trace.update({
         state: Trace.state.REQUEST_ANSWERED,
-        answer: 'accepted'
-      })
+        answer: 'accepted',
+      });
       return res.sendStatus(200);
-    }
+    };
 
     const sendError = (status, message) => {
       if (!req.body) req.body = {};
       logger.log({ success: false, url: req.body.url, address: req.body.address, status: status, message: message });
       trace.update({
         state: Trace.state.REQUEST_ANSWERED,
-        answer: 'rejected'
-      })
+        answer: 'rejected',
+      });
       return res.status(status).send({ error: message });
-    }
-
+    };
 
     // Basic sanity check
     if (!req.body) return sendError(400, 'no request body found');
     trace.update({
       state: Trace.state.BODY_RECEIVED,
       body: req.body,
-    })
+    });
     if (!req.body.url) return sendError(400, 'url not found in body');
-    if (!req.body.address) return sendError( 400, 'address not found in body');
+    if (!req.body.address) return sendError(400, 'address not found in body');
     if (!ae || !ae.client) return sendError(500, 'sdk not initialized yet');
     trace.update({
       state: Trace.state.DATA_PARSED,
       url: req.body.url,
-      address: req.body.address
-    })
+      address: req.body.address,
+    });
+    trace.setUrl(req.body.url);
 
     // Try to claim
     try {
@@ -57,9 +57,9 @@ module.exports = class PayForTxLogic {
       const result = await ae.checkPreClaim(req.body.address, req.body.url, trace);
 
       // run claim async
-      PayForTxLogic.runAsyncClaim(req.body.address, req.body.url, trace)
+      PayForTxLogic.runAsyncClaim(req.body.address, req.body.url, trace);
 
-      if(!result) return sendError(400, 'Claim rejected');
+      if (!result) return sendError(400, 'Claim rejected');
       return sendSuccess();
     } catch (e) {
       console.error(e);
@@ -67,20 +67,20 @@ module.exports = class PayForTxLogic {
     }
   }
 
-  static async runAsyncClaim(address, url, trace) {
+  static async runAsyncClaim (address, url, trace) {
     try {
-      await ae.claimTips(address, url, trace)
+      await ae.claimTips(address, url, trace);
       CacheLogic.invalidateTips();
       CacheLogic.invalidateOracle();
       CacheLogic.invalidateContractEvents();
       trace.finished({
-        result: 'success'
-      })
+        result: 'success',
+      });
     } catch (e) {
       trace.update({ state: Trace.state.CAUGHT_ERROR, error: e.message });
       trace.finished({
-        result: 'error'
-      })
+        result: 'error',
+      });
     }
   }
 

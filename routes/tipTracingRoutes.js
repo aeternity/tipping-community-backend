e8f5a6e98d2cb484ee01577ca0017ae65ce1c2f0
Philipp Piwo <github@piwo.space>
2020-05-14 16:53:40 +0200

    rely on external init

--- routes/tipTracingRoutes.js
+++ routes/tipTracingRoutes.js
@@ -3,8 +3,6 @@ const Router = require('express').Router;
 
 const router = new Router();
 
-new TipTracing(); //calls init
-
 // Open api routes
 router.get('/blockchain', TipTracing.fetchBlockchainTrace);
 

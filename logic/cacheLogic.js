ca2575da784ef3b333b8043972414331edfa3206
kenodressel <hire@keno.digital>
2020-05-27 15:56:54 +0200

    removes getRandom function

--- logic/cacheLogic.js
+++ logic/cacheLogic.js
@@ -78,21 +78,6 @@ module.exports = class CacheLogic {
     });
     let result = [];
 
-
-    function getRandom(arr, n) {
-      var result = new Array(n),
-        len = arr.length,
-        taken = new Array(len);
-      if (n > len)
-        throw new RangeError("getRandom: more elements taken than available");
-      while (n--) {
-        var x = Math.floor(Math.random() * len);
-        result[n] = arr[x in taken ? taken[x] : x];
-        taken[x] = --len in taken ? taken[len] : len;
-      }
-      return result;
-    }
-
     await lock.acquire("CacheLogic.fetchAllLanguages", async () => {
       const languages = await Tip.findAll({raw: true});
       const tipIds = [...new Set(tips.map(tip => tip.id))];

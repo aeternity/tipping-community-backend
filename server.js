a31b3cff3c9da957cad8726452d07edf047d00c4
Philipp Piwo <github@piwo.space>
2020-06-29 11:28:03 +0200

    extract token cache routes and logic

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use('/comment', require('./routes/commentRoutes.js'));
 app.use('/linkpreview', require('./routes/linkPreviewRoutes.js'));
 app.use('/verified', require('./routes/verifiedRoutes.js'));
 app.use('/cache', require('./routes/cacheRoutes.js'));
+app.use('/tokenCache', require('./routes/tokenCacheRoutes.js'));
 app.use('/claim', require('./routes/payForTxRoutes.js'));
 app.use('/static', require('./routes/staticRoutes.js'));
 app.use('/profile', require('./routes/profileRoutes.js'));

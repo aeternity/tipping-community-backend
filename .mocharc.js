c36deb55844f2fdabfc6d049412684288f2183f0
Philipp Piwo <github@piwo.space>
2020-07-03 09:22:06 +0200

    use testnet for tests

--- .mocharc.js
+++ .mocharc.js
@@ -1,18 +1,19 @@
 /*
 process.env = {
   ...process.env,
-  NODE_URL: 'https://mainnet.aeternity.io',
-  COMPILER_URL: 'https://compiler.aepps.com',
-  CONTRACT_ADDRESS: 'ct_2AfnEfCSZCTEkxL5Yoi4Yfq6fF7YapHRaFKDJK3THMXMBspp5z',
-  ORACLE_CONTRACT_ADDRESS: 'ct_2VpQ1QGXy7KA2rsQmC4QraFKwQam3Ksqq3cAK8KHUNwhoiQkL',
+  NODE_URL: 'https://testnet.aeternity.io',
+  COMPILER_URL: 'https://latest.compiler.aepps.com',
+  MIDDLEWARE_URL: 'https://testnet.aeternity.io',
+
+  OLD_CONTRACT_ADDRESS: 'ct_2siQwATqx7TFPr6D7nj4SqAGmkurVSBvRvL8xUfRPS5VZD4y4Y',
+  CONTRACT_ADDRESS: 'ct_27MiRFPEvT4Ep7Go2P1KL3Gxo3TSDb9s6WjCkc6bkDuy74zqJt',
+  ORACLE_CONTRACT_ADDRESS: 'ct_2X9ThTUvPX6uSrCUFinsELW4K2G25eZjUXCScaGihfezuvigxn',
+
   AUTHENTICATION_USER: 'admin',
   AUTHENTICATION_PASSWORD: 'pass',
-  CONTRACT_FILE: 'NewTippingInterface',
   NODE_ENV: 'test',
   REDIS_URL: 'redis://localhost:6379',
-  IPFS_URL: 'http://localhost:5001',
-  //IPFS_URL: 'http://3.124.114.189:5001',
-  MIDDLEWARE_URL: 'https://mainnet.aeternity.io'
+  IPFS_URL: 'http://localhost:5001'
 };
 */
 process.env = {

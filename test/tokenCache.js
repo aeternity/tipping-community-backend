cd9efd3ca72fdd86c5dc5b6fc028df6f59e6c603
Philipp Piwo <github@piwo.space>
2020-07-06 11:36:21 +0200

    better test token cache routes

--- test/tokenCache.js
+++ test/tokenCache.js
@@ -7,9 +7,17 @@ chai.use(chaiHttp);
 describe('Token Cache', () => {
   describe('API', () => {
     it('it should GET token info', function (done) {
+      this.timeout(30000);
+
       chai.request(server).get('/tokenCache/tokenInfo').end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
+        res.body.should.have.property('ct_2DQ1vdJdiaNVgh2vUbTTpkPRiT9e2GSx1NxyU7JM9avWqj6dVf');
+        res.body['ct_2DQ1vdJdiaNVgh2vUbTTpkPRiT9e2GSx1NxyU7JM9avWqj6dVf'].should.be.deep.equal({
+          "decimals": 18,
+          "name": "Sample Test Token",
+          "symbol": "STT"
+        })
         done();
       });
     });
@@ -23,12 +31,20 @@ describe('Token Cache', () => {
       });
     });
 
-    it('it should GET token info', function (done) {
+    it('it shouldnt GET token info without address', function (done) {
+      chai.request(server).get('/tokenCache/balances').end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+    });
+
+    it('it should GET token balances for address', function (done) {
       chai.request(server).get('/tokenCache/balances?address=' + process.env.PUBLIC_KEY).end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
         done();
       });
     });
+
   });
 });

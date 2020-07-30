cacbe8e2521cbb11cddddc624ef16d40452bf99c
kenodressel <hire@keno.digital>
2020-07-30 11:29:22 +0200

    adds base for claim notifications

--- logic/retipLogic.js
+++ logic/retipLogic.js
@@ -1,4 +1,5 @@
 const { Retip } = require('../models');
+const NotificationLogic = require('./notificationLogic');
 
 module.exports = class RetipLogic {
   static async fetchAllLocalRetips() {
@@ -7,5 +8,24 @@ module.exports = class RetipLogic {
 
   static async bulkCreate(tips) {
     return Retip.bulkCreate(tips, { raw: true });
+  }
+
+  static async updateRetipsDB(remoteTips) {
+    const localRetips = await RetipLogic.fetchAllLocalRetips();
+    const remoteRetips = [...new Set(remoteTips.map(tip => tip.retips.map(retip => ({ ...retip, parentTip: tip }))).flat())];
+    const remoteRetipIds = [...new Set(remoteRetips.map(retip => retip.id))];
+    const localRetipIds = [...new Set(localRetips.map(retip => retip.id))];
+
+    const newReTipIds = remoteRetipIds.filter(id => !localRetipIds.includes(id));
+    const oldReTipIds = remoteRetipIds.filter(id => localRetipIds.includes(id));
+
+    // Send appropriate notifications for new tips
+    await newReTipIds.asyncMap(id => NotificationLogic.handleNewRetip(remoteRetips.find(retip => retip.id === id)));
+    await oldReTipIds.asyncMap(id => NotificationLogic.handleOldRetip(remoteRetips.find(retip => retip.id === id)));
+
+    await RetipLogic.bulkCreate(
+      newReTipIds.map(id => remoteRetips.find(({ id: retipId }) => id === retipId))
+        .map(({ id, parentTip, claim }) => ({ id, tipId: parentTip.id, unclaimed: claim.unclaimed })),
+    );
   }
 };

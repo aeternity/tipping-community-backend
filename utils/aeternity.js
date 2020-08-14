da8987222862c3b4f60837ca74cb09ecdc87efc4
Philipp Piwo <github@piwo.space>
2020-08-14 17:10:56 +0200

    prepare for simultaneous v1, v2 contract use

--- utils/aeternity.js
+++ utils/aeternity.js
@@ -1,7 +1,8 @@
 const { Universal, Node, MemoryAccount } = require('@aeternity/aepp-sdk');
 const requireESM = require('esm')(module); // use to handle es6 import/export
 const axios = require('axios');
-const TIPPING_INTERFACE = require('tipping-contract/TippingInterface.aes');
+const TIPPING_V1_INTERFACE = require('tipping-contract/Tipping_v1_Interface.aes');
+const TIPPING_V2_INTERFACE = require('tipping-contract/Tipping_v2_Interface.aes');
 const ORACLE_SERVICE_INTERFACE = require('tipping-oracle-service/OracleServiceInterface.aes');
 const TOKEN_CONTRACT_INTERFACE = require('aeternity-fungible-token/FungibleTokenFullInterface.aes');
 const TOKEN_REGISTRY = require('token-registry/TokenRegistry.aes');
@@ -39,7 +40,8 @@ class Aeternity {
         compilerUrl: process.env.COMPILER_URL,
       });
 
-      this.contract = await this.client.getContractInstance(TIPPING_INTERFACE, { contractAddress: process.env.CONTRACT_ADDRESS });
+      this.contractV1 = await this.client.getContractInstance(TIPPING_V1_INTERFACE, { contractAddress: process.env.OLD_CONTRACT_ADDRESS });
+      this.contractV2 = await this.client.getContractInstance(TIPPING_V2_INTERFACE, { contractAddress: process.env.CONTRACT_ADDRESS });
       this.oracleContract = await this.client.getContractInstance(
         ORACLE_SERVICE_INTERFACE,
         { contractAddress: process.env.ORACLE_CONTRACT_ADDRESS },
@@ -159,8 +161,9 @@ class Aeternity {
 
   async getTips() {
     const fetchTips = async () => {
-      const state = await this.contract.methods.get_state();
-      const { tips } = tippingContractUtil.getTipsRetips(state.decodedResult);
+      const fetchV1State = this.contractV1.methods.get_state();
+      const fetchV2State = this.contractV2.methods.get_state();
+      const { tips } = tippingContractUtil.getTipsRetips(await fetchV1State, await fetchV2State);
       return this.addAdditionalTipsData(tips);
     };
 
@@ -227,7 +230,9 @@ class Aeternity {
     trace.update({
       state: TRACE_STATES.STARTED_PRE_CLAIM,
     });
-    const claimAmount = await this.contract.methods.unclaimed_for_url(url).then(r => r.decodedResult).catch(trace.catchError(0));
+
+    //TODO update for v1 v2 simulataneous
+    const claimAmount = await this.contractV2.methods.unclaimed_for_url(url).then(r => r.decodedResult).catch(trace.catchError(0));
 
     trace.update({
       state: TRACE_STATES.CLAIM_AMOUNT,
@@ -242,14 +247,16 @@ class Aeternity {
     await this.checkPreClaim(address, url, trace);
 
     // pre-claim if necessary (if not already claimed successfully)
-    const claimSuccess = await this.contract.methods.check_claim(url, address).then(r => r.decodedResult.success).catch(trace.catchError(false));
+    //TODO update for v1 v2 simulataneous
+    const claimSuccess = await this.contractV2.methods.check_claim(url, address).then(r => r.decodedResult.success).catch(trace.catchError(false));
 
     trace.update({ state: TRACE_STATES.INITIAL_PRECLAIM_RESULT, claimSuccess });
 
     if (!claimSuccess) {
       const fee = await this.oracleContract.methods.estimate_query_fee();
       trace.update({ state: TRACE_STATES.ESTIMATED_FEE, fee: fee.decodedResult });
-      await this.contract.methods.pre_claim(url, address, { amount: fee.decodedResult });
+      //TODO update for v1 v2 simulataneous
+      await this.contractV2.methods.pre_claim(url, address, { amount: fee.decodedResult });
       trace.update({ state: TRACE_STATES.PRECLAIM_STARTED });
 
       return new Promise((resolve, reject) => {
@@ -258,7 +265,8 @@ class Aeternity {
         let interval = null;
 
         const checkPreClaimFinished = async () => {
-          if ((await this.contract.methods.check_claim(url, address)).decodedResult.success) {
+          //TODO update for v1 v2 simulataneous
+          if ((await this.contractV2.methods.check_claim(url, address)).decodedResult.success) {
             clearInterval(interval);
             return resolve();
           }
@@ -281,7 +289,8 @@ class Aeternity {
     try {
       const claimSuccess = await this.preClaim(address, url, trace);
       trace.update({ state: TRACE_STATES.FINAL_PRECLAIM_RESULT, claimSuccess });
-      const result = await this.contract.methods.claim(url, address, false);
+      //TODO update for v1 v2 simulataneous
+      const result = await this.contractV2.methods.claim(url, address, false);
       trace.update({ state: TRACE_STATES.CLAIM_RESULT, tx: result, result: result.decodedResult });
       return result.decodedResult;
     } catch (e) {

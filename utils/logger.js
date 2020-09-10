f205ece19bc8d1e6efd514977690380707042d96
kenodressel <hire@keno.digital>
2020-09-10 18:00:14 +0200

    fixes colors in log files

--- utils/logger.js
+++ utils/logger.js
@@ -23,52 +23,58 @@ const enumerateErrorFormat = winston.format(info => {
   return info;
 });
 
+const customFormatter = fileName => msg => {
+  const splatMsg = msg[Symbol.for('splat')] ? msg[Symbol.for('splat')].join(' ') : '';
+  // ALL LOGS
+  if (msg[Symbol.for('level')] !== 'error') return `[${msg.timestamp}] (${fileName}) ${msg.level}: ${msg.message} ${splatMsg} `;
+  // ONLY ERROR
+  const prefix = `[${msg.timestamp}] (${fileName}) ${msg.level}:`;
+  const logs = [];
+  // SEQUELIZE ERRORS
+  if (msg.sql) {
+    logs.push(`${msg.name} (${msg.original.message}) ${splatMsg} `);
+    logs.push(`${msg.original.detail} ${splatMsg} `);
+  } else if (msg.stack) {
+    // NORMAL ERROR
+    logs.push(msg.message);
+    logs.push(...msg.stack.split('\n').reduce((acc, curr) => {
+      if (acc[acc.length - 1] !== curr) acc.push(curr);
+      return acc;
+    }, []));
+  } else {
+    // CUSTOM ERROR MESSAGE
+    logs.push(msg.message);
+  }
+
+  return logs.map(line => `${prefix} ${line}`).join('\n');
+};
+
 const logger = module => {
   const fileName = path.basename(module.id, '.js');
   return winston.createLogger({
     format: winston.format.combine(
       enumerateErrorFormat(),
       winston.format.timestamp(),
-      winston.format.colorize(),
-      winston.format.printf(msg => {
-        const splatMsg = msg[Symbol.for('splat')] ? msg[Symbol.for('splat')].join(' ') : '';
-        // ALL LOGS
-        if (msg[Symbol.for('level')] !== 'error') return `[${msg.timestamp}] (${fileName}) ${msg.level}: ${msg.message} ${splatMsg} `;
-        // ONLY ERROR
-        const prefix = `[${msg.timestamp}] (${fileName}) ${msg.level}:`;
-        const logs = [];
-        // SEQUELIZE ERRORS
-        if (msg.sql) {
-          logs.push(`${msg.name} (${msg.original.message}) ${splatMsg} `);
-          logs.push(`${msg.original.detail} ${splatMsg} `);
-        } else if (msg.stack) {
-          // NORMAL ERROR
-          logs.push(msg.message);
-          logs.push(...msg.stack.split('\n').reduce((acc, curr) => {
-            if (acc[acc.length - 1] !== curr) acc.push(curr);
-            return acc;
-          }, []));
-        } else {
-          // CUSTOM ERROR MESSAGE
-          logs.push(msg.message);
-        }
-
-        return logs.map(line => `${prefix} ${line}`).join('\n');
-      }),
     ),
     transports: [
       new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.printf(customFormatter(fileName)),
+        ),
         level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
       }),
       new (winston.transports.DailyRotateFile)({
-        filename: 'combined-%DATE%.log',
-        datePattern: 'YYYY-MM-DD-HH',
+        format: winston.format.printf(customFormatter(fileName)),
+        filename: './logs/combined-%DATE%.log',
+        datePattern: 'YYYY-MM-DD',
         zippedArchive: true,
         maxSize: '20m',
         maxFiles: '14d',
         level: 'info',
       }),
       new winston.transports.File({
+        format: winston.format.printf(customFormatter(fileName)),
         filename: './logs/errors.log',
         level: 'error',
       }),

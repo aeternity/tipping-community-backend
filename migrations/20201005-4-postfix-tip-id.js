a428b5c460bad10a3c885819b8446f1b423a4e09
kenodressel <hire@keno.digital>
2020-10-05 15:39:52 +0200

    removes try catch from migration

--- migrations/20201005-4-postfix-tip-id.js
+++ migrations/20201005-4-postfix-tip-id.js
@@ -24,18 +24,13 @@ module.exports = {
     // DROP NOTIFICATIONS SO ALL ENTRIES CONTAIN A SENDER
     const transaction = await queryInterface.sequelize.transaction();
     // MODIFY DATA
-    try {
-      await queryInterface.sequelize.query('UPDATE "BlacklistEntries" SET "tipId" = "tipId" || \'_v1\';', { transaction: transaction });
-      await queryInterface.sequelize.query('UPDATE "Comments" SET "tipId" = "tipId" || \'_v1\';', { transaction: transaction });
-      await queryInterface.sequelize.query('UPDATE "Tips" SET "id" = "id" || \'_v1\' ;', { transaction: transaction }); // Should cascade
-      await queryInterface.sequelize.query('UPDATE "Retips" SET "id" = "id" || \'_v1\';', { transaction: transaction });
-      await queryInterface.sequelize.query('UPDATE "Notifications" SET "entityId" = "entityId" || \'_v1\' WHERE "entityType" = \'TIP\';', { transaction: transaction });
-      await queryInterface.sequelize.query('UPDATE "Notifications" SET "sourceId" = "sourceId" || \'_v1\' WHERE "sourceType" = \'TIP\' OR "sourceType" = \'RETIP\';', { transaction: transaction });
-      return transaction.commit();
-    } catch (e) {
-      console.error(e)
-    }
-
+    await queryInterface.sequelize.query('UPDATE "BlacklistEntries" SET "tipId" = "tipId" || \'_v1\';', { transaction: transaction });
+    await queryInterface.sequelize.query('UPDATE "Comments" SET "tipId" = "tipId" || \'_v1\';', { transaction: transaction });
+    await queryInterface.sequelize.query('UPDATE "Tips" SET "id" = "id" || \'_v1\' ;', { transaction: transaction }); // Should cascade
+    await queryInterface.sequelize.query('UPDATE "Retips" SET "id" = "id" || \'_v1\';', { transaction: transaction });
+    await queryInterface.sequelize.query('UPDATE "Notifications" SET "entityId" = "entityId" || \'_v1\' WHERE "entityType" = \'TIP\';', { transaction: transaction });
+    await queryInterface.sequelize.query('UPDATE "Notifications" SET "sourceId" = "sourceId" || \'_v1\' WHERE "sourceType" = \'TIP\' OR "sourceType" = \'RETIP\';', { transaction: transaction });
+    return transaction.commit();
   },
   down: () => {},
   info: info

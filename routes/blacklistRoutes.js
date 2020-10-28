5dcbc7a9679d5d62ec57a72a38eb5fb8e5230fb9
kenodressel <hire@keno.digital>
2020-10-28 14:14:59 +0100

    updates swagger definitions

--- routes/blacklistRoutes.js
+++ routes/blacklistRoutes.js
@@ -10,31 +10,152 @@ const router = new Router();
  * @swagger
  * /blacklist/api:
  *   get:
- *     description: Returns all blacklisted items
- *     produces:
- *       - application/json
+ *     summary: Returns all blacklisted items
+ *     responses:
+ *       200:
+ *         description: Returns all blacklisted items
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/BlacklistEntry'
+ */
+router.get('/api', Logic.getAllItems);
+/**
+ * @swagger
+ * /blacklist/api/{tipId}:
+ *   get:
+ *     summary: Returns blacklist status for a tipId
+ *     parameters:
+ *       - in: path
+ *         name: tipId
+ *         required: true
+ *         schema:
+ *           type: string
  *     responses:
  *       200:
  *         description: blacklisted tips
- *         schema:
- *           oneOf:
- *            - $ref: '#/components/schemas/SignatureRequest'
- *            - $ref: '#/components/schemas/BlacklistEntry'
+ *         content:
+ *           application/json:
+ *             schema:
+ *              $ref: '#/components/schemas/BlacklistEntry'
  */
-router.get('/api', Logic.getAllItems);
 router.get('/api/:tipId', Logic.getSingleItem);
 
 // View routes
+/**
+ * @swagger
+ * /blacklist/:
+ *   get:
+ *     summary: Returns Userinterface to flag / unflag / remove tips
+ *     security:
+ *       - basicAuth: []
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           text/html:
+ *             schema:
+ *               type: string
+ */
 router.get('/', basicAuth, async (req, res) => res.render('admin', {
   allItems: await Logic.augmentAllItems(await CacheLogic.getTips()),
 }));
 
 // Restricted api routes
+/**
+ * @swagger
+ * /blacklist/api:
+ *   post:
+ *     summary: Adds tip to blacklist
+ *     security:
+ *       - basicAuth: []
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/BlacklistEntry'
+ *     responses:
+ *       200:
+ *         description: created blacklist entry
+ *         content:
+ *           application/json:
+ *             schema:
+ *              $ref: '#/components/schemas/BlacklistEntry'
+ */
 router.post('/api', basicAuth, Logic.addItem);
+/**
+ * @swagger
+ * /blacklist/api/{tipId}:
+ *   put:
+ *     summary: Updates status of blacklisted tip
+ *     security:
+ *       - basicAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: tipId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/BlacklistEntry'
+ *     responses:
+ *       200:
+ *         description: updated blacklist entry
+ *         content:
+ *           application/json:
+ *             schema:
+ *              $ref: '#/components/schemas/BlacklistEntry'
+ */
 router.put('/api/:tipId', basicAuth, Logic.updateItem);
+/**
+ * @swagger
+ * /blacklist/api/{tipId}:
+ *   delete:
+ *     summary: Removes tip from blacklist
+ *     security:
+ *       - basicAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: tipId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ */
 router.delete('/api/:tipId', basicAuth, Logic.removeItem);
 
 // Public routes
+/**
+ * @swagger
+ * /blacklist/api/wallet:
+ *   post:
+ *     summary: Adds tip to blacklist
+ *     security:
+ *       - signatureAuth: []
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             oneOf:
+ *               - $ref: '#/components/schemas/BlacklistEntry'
+ *               - $ref: '#/components/schemas/SignatureRequest'
+ *     responses:
+ *       200:
+ *         description: created blacklist entry
+ *         content:
+ *           application/json:
+ *             schema:
+ *               oneOf:
+ *                - $ref: '#/components/schemas/BlacklistEntry'
+ *                - $ref: '#/components/schemas/SignatureResponse'
+ */
 router.post('/api/wallet', signatureAuth, Logic.flagTip);
 
 module.exports = router;

af4c1e79da7e17385fb291112000eeb020da19b6
kenodressel <hire@keno.digital>
2020-11-02 19:01:47 +0100

    documents tokenCache & verified

--- routes/verifiedRoutes.js
+++ routes/verifiedRoutes.js
@@ -4,6 +4,22 @@ const Logic = require('../logic/verifiedLogic.js');
 const router = new Router();
 
 // Open api routes
+/**
+ * @swagger
+ * /verified/:
+ *   get:
+ *     summary: Returns a list of domains where claiming tips has already worked
+ *     responses:
+ *       200:
+ *         description: Returns a list of domains where claiming tips has already worked
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: string
+ *                 format: url
+ */
 router.get('/', Logic.getAllClaimedEvents);
 
 module.exports = router;

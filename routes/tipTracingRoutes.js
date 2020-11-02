13355cb05fb52caf7f86314248ac45c6076438f8
kenodressel <hire@keno.digital>
2020-11-02 18:49:22 +0100

    adds static and tracing docs

--- routes/tipTracingRoutes.js
+++ routes/tipTracingRoutes.js
@@ -4,7 +4,47 @@ const TipTracing = require('../logic/tipTracingLogic.js');
 const router = new Router();
 
 // Open api routes
+/**
+ * @swagger
+ * /tracing/backend:
+ *   get:
+ *     summary: Returns all traces for a given tipid
+ *     parameters:
+ *       - in: query
+ *         schema:
+ *           type: string
+ *         description: A tipId
+ *         name: id
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Returns all traces for a given tipid
+ *         content:
+ *           application/json:
+ *             schema:
+ *              $ref: '#/components/schemas/Trace'
+ */
 router.get('/backend', TipTracing.getAllTraces);
+/**
+ * @swagger
+ * /tracing/blockchain:
+ *   get:
+ *     summary: Returns all traces for a given tipid
+ *     parameters:
+ *       - in: query
+ *         schema:
+ *           type: string
+ *         description: A tipId
+ *         name: id
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Returns all traces for a given tipid
+ *         content:
+ *           application/json:
+ *             schema:
+ *              $ref: '#/components/schemas/Trace'
+ */
 router.get('/blockchain', TipTracing.fetchBlockchainTrace);
 
 module.exports = router;

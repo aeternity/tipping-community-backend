157d93c771a45be96cdc030876f46966fd857090
kenodressel <hire@keno.digital>
2020-11-02 18:49:22 +0100

    adds static and tracing docs

--- routes/staticRoutes.js
+++ routes/staticRoutes.js
@@ -4,7 +4,89 @@ const StaticLogic = require('../logic/staticLogic.js');
 const router = new Router();
 
 // Open api routes
+/**
+ * @swagger
+ * /static/stats:
+ *   get:
+ *     summary: Returns aggregated stats over timeperiodes
+ *     responses:
+ *       200:
+ *         description: Returns aggregated stats over timeperiodes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 comments:
+ *                   type: object
+ *                   properties:
+ *                     today:
+ *                       type: integer
+ *                     yesterday:
+ *                       type: integer
+ *                     last7Days:
+ *                       type: integer
+ *                     last30Days:
+ *                       type: integer
+ *                     total:
+ *                       type: integer
+ *                 linkPreviews:
+ *                   type: object
+ *                   properties:
+ *                     today:
+ *                       type: integer
+ *                     yesterday:
+ *                       type: integer
+ *                     last7Days:
+ *                       type: integer
+ *                     last30Days:
+ *                       type: integer
+ *                     total:
+ *                       type: integer
+ *                 profiles:
+ *                   type: object
+ *                   properties:
+ *                     today:
+ *                       type: integer
+ *                     yesterday:
+ *                       type: integer
+ *                     last7Days:
+ *                       type: integer
+ *                     last30Days:
+ *                       type: integer
+ *                     total:
+ *                       type: integer
+ *                 blacklist:
+ *                   type: object
+ *                   properties:
+ *                     today:
+ *                       type: integer
+ *                     yesterday:
+ *                       type: integer
+ *                     last7Days:
+ *                       type: integer
+ *                     last30Days:
+ *                       type: integer
+ *                     total:
+ *                       type: integer
+ */
 router.get('/stats', StaticLogic.deliverStats);
+/**
+ * @swagger
+ * /static/wallet/graylist:
+ *   get:
+ *     summary: Returns a list of domains where claiming tips can be troublesome
+ *     responses:
+ *       200:
+ *         description: Returns a list of domains where claiming tips can be troublesome
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: string
+ *                 format: url
+ */
 router.get('/wallet/graylist', StaticLogic.getGrayList);
 
 module.exports = router;

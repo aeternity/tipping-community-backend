b57185699c908628f8b6debe5f276a352aa52602
kenodressel <hire@keno.digital>
2020-11-25 17:45:20 +0100

    chore(*): fixes db & tests

--- models/index.js
+++ models/index.js
@@ -1,18 +1,17 @@
-const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 require('sequelize-hierarchy')(Sequelize);
+const glob = require('glob');
 const config = require('../config/config.js');
 
 const basename = path.basename(__filename);
 const db = {};
 
 const sequelize = new Sequelize(config.development);
-fs
-  .readdirSync(__dirname)
+glob.sync('modules/**/models/*.js')
   .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach(file => {
-    const model = sequelize.import(path.join(__dirname, file));
+    const model = sequelize.import(path.join(__dirname, '..', file));
     db[model.name] = model;
   });
 

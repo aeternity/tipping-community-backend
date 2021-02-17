c0dd99462f65ec3b9ce702d1e4c3c4af04655607
kenodressel <hire@keno.digital>
2021-02-17 16:42:50 +0100

    refactor(blacklist): moves from class to module

--- modules/blacklist/logic/blacklistLogic.js
+++ modules/blacklist/logic/blacklistLogic.js
@@ -1,88 +1,65 @@
-const cache = require('../../cache/utils/cache');
+const CacheLogic = require('../../cache/logic/cacheLogic');
 const { BlacklistEntry } = require('../../../models');
 const { BLACKLIST_STATUS } = require('../constants/blacklistStates');
 
-module.exports = class Blacklist {
-  static async augmentAllItems(allItems) {
-    const blacklist = await BlacklistEntry.findAll({
-      raw: true,
-    });
-    return allItems.map(item => ({
-      ...item,
-      hidden: blacklist.some(b => b.tipId === item.id && b.status === BLACKLIST_STATUS.HIDDEN),
-      flagged: blacklist.some(b => b.tipId === item.id && b.status === BLACKLIST_STATUS.FLAGGED),
-    })).sort((a, b) => b.timestamp - a.timestamp);
-  }
+async function augmentAllItems(allItems) {
+  const blacklist = await BlacklistEntry.findAll({
+    raw: true,
+  });
+  return allItems.map(item => ({
+    ...item,
+    hidden: blacklist.some(b => b.tipId === item.id && b.status === BLACKLIST_STATUS.HIDDEN),
+    flagged: blacklist.some(b => b.tipId === item.id && b.status === BLACKLIST_STATUS.FLAGGED),
+  })).sort((a, b) => b.timestamp - a.timestamp);
+}
 
-  static async addItem(req, res) {
-    try {
-      const { tipId } = req.body;
-      if (!tipId) return res.status(400).send('Missing required field tipId');
-      const entry = await BlacklistEntry.create({ tipId: String(tipId) });
-      // Kill stats cache
-      await cache.del(['StaticLogic.getStats']);
-      await cache.del(['CacheLogic.getAllTips', 'blacklisted']);
-      return res.send(entry);
-    } catch (e) {
-      return res.status(500).send(e.message);
-    }
-  }
+async function resetCache() {
+  await CacheLogic.invalidateBlacklistedTips();
+  await CacheLogic.invalidateStatsCache();
+}
 
-  static async flagTip(req, res) {
-    try {
-      let { tipId } = req.body;
-      const { author } = req.body;
-      // Wallet sends tip as number
-      tipId = String(tipId);
-      if (!tipId) return res.status(400).send('Missing required field tipId');
-      if (!author) return res.status(400).send('Missing required field author');
-      let existingEntry = await BlacklistEntry.findOne({ where: { tipId } });
-      if (!existingEntry) {
-        existingEntry = await BlacklistEntry.create({ tipId, flagger: author, status: BLACKLIST_STATUS.FLAGGED });
-        // Kill stats cache
-        await cache.del(['StaticLogic.getStats']);
-        await cache.del(['CacheLogic.getAllTips', 'blacklisted']);
-      }
-      return res.send(existingEntry.toJSON());
-    } catch (e) {
-      return res.status(500).send(e.message);
-    }
-  }
+async function addItem(tipId) {
+  const entry = await BlacklistEntry.create({ tipId: String(tipId) });
 
-  static async updateItem(req, res) {
-    try {
-      const { status } = req.body;
-      const { tipId } = req.params;
-      if (!tipId) return res.status(400).send('Missing required field tipId');
-      if (!status) return res.status(400).send('Missing required field status');
-      await BlacklistEntry.update({ status }, { where: { tipId } });
-      return res.sendStatus(200);
-    } catch (e) {
-      return res.status(500).send(e.message);
-    }
-  }
+  await resetCache();
+  return entry;
+}
 
-  static async removeItem(req, res) {
-    const result = await BlacklistEntry.destroy({
-      where: {
-        tipId: req.params.tipId,
-      },
-    });
-    await cache.del(['CacheLogic.getAllTips', 'blacklisted']);
-    return result === 1 ? res.sendStatus(200) : res.sendStatus(404);
+async function flagTip(tipId, author) {
+  let entry = await BlacklistEntry.findOne({ where: { tipId }, raw: true });
+  if (!entry) {
+    entry = await BlacklistEntry.create({ tipId, flagger: author, status: BLACKLIST_STATUS.FLAGGED });
+    // Kill stats cache
+    await resetCache();
   }
+  return entry;
+}
 
-  static async getAllItems(req, res) {
-    res.send(await BlacklistEntry.findAll({ raw: true }));
-  }
+async function updateItem(tipId, status) {
+  await BlacklistEntry.update({ status }, { where: { tipId } });
+  await resetCache();
+}
 
-  static async getSingleItem(req, res) {
-    const result = await BlacklistEntry.findOne({ where: { tipId: req.params.tipId } });
-    return result ? res.send(result.toJSON()) : res.sendStatus(404);
-  }
+async function removeItem(tipId) {
+  const result = await BlacklistEntry.destroy({
+    where: {
+      tipId,
+    },
+  });
+  await resetCache();
+  return result;
+}
 
-  static async getBlacklistedIds() {
-    const blacklist = await BlacklistEntry.findAll({ raw: true, where: { status: BLACKLIST_STATUS.HIDDEN } });
-    return blacklist.map(b => b.tipId);
-  }
+async function getBlacklistedIds() {
+  const blacklist = await BlacklistEntry.findAll({ raw: true, where: { status: BLACKLIST_STATUS.HIDDEN } });
+  return blacklist.map(b => b.tipId);
+}
+
+module.exports = {
+  augmentAllItems,
+  addItem,
+  flagTip,
+  updateItem,
+  removeItem,
+  getBlacklistedIds,
 };

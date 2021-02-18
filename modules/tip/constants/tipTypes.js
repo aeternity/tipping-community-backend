d9a549fb227b0a965352798ec25c9408a4037c09
Philipp Piwo <github@piwo.space>
2021-02-18 18:40:20 +0100

    feat(tipdb): persist additional tip fields in db

--- modules/tip/constants/tipTypes.js
+++ modules/tip/constants/tipTypes.js
@@ -3,8 +3,26 @@ const TIP_TYPES = {
   TOKEN_TIP: 'TOKEN_TIP',
   DIRECT_AE_TIP: 'DIRECT_AE_TIP',
   DIRECT_TOKEN_TIP: 'DIRECT_TOKEN_TIP',
+  POST_WITHOUT_TIP: 'POST_WITHOUT_TIP',
+};
+
+const mapTipType = type => {
+  switch (type) {
+    case 'AeTip':
+      return TIP_TYPES.AE_TIP;
+    case 'TokenTip':
+      return TIP_TYPES.TOKEN_TIP;
+    case 'DirectAeTip':
+      return TIP_TYPES.DIRECT_AE_TIP;
+    case 'DirectTokenTip':
+      return TIP_TYPES.DIRECT_TOKEN_TIP;
+    case 'PostWithoutTip':
+      return TIP_TYPES.POST_WITHOUT_TIP;
+    default:
+      throw Error(`unknown tip type ${type}`);
+  }
 };
 
 module.exports = {
-  TIP_TYPES,
+  TIP_TYPES, mapTipType,
 };

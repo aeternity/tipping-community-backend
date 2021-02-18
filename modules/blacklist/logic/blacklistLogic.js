64574ad08b9b421a7f6d10fd63aef63259fb693e
kenodressel <hire@keno.digital>
2021-02-18 12:04:46 +0100

    refactor(blacklist): creates singleton

--- modules/blacklist/logic/blacklistLogic.js
+++ modules/blacklist/logic/blacklistLogic.js
@@ -2,64 +2,59 @@ const CacheLogic = require('../../cache/logic/cacheLogic');
 const { BlacklistEntry } = require('../../../models');
 const { BLACKLIST_STATUS } = require('../constants/blacklistStates');
 
-async function augmentAllItems(allItems) {
-  const blacklist = await BlacklistEntry.findAll({
-    raw: true,
-  });
-  return allItems.map(item => ({
-    ...item,
-    hidden: blacklist.some(b => b.tipId === item.id && b.status === BLACKLIST_STATUS.HIDDEN),
-    flagged: blacklist.some(b => b.tipId === item.id && b.status === BLACKLIST_STATUS.FLAGGED),
-  })).sort((a, b) => b.timestamp - a.timestamp);
-}
+const BlacklistLogic = {
+  async augmentAllItems(allItems) {
+    const blacklist = await BlacklistEntry.findAll({
+      raw: true,
+    });
+    return allItems.map(item => ({
+      ...item,
+      hidden: blacklist.some(b => b.tipId === item.id && b.status === BLACKLIST_STATUS.HIDDEN),
+      flagged: blacklist.some(b => b.tipId === item.id && b.status === BLACKLIST_STATUS.FLAGGED),
+    })).sort((a, b) => b.timestamp - a.timestamp);
+  },
 
-async function resetCache() {
-  await CacheLogic.invalidateBlacklistedTips();
-  await CacheLogic.invalidateStatsCache();
-}
+  async resetCache() {
+    await CacheLogic.invalidateBlacklistedTips();
+    await CacheLogic.invalidateStatsCache();
+  },
 
-async function addItem(tipId) {
-  const entry = await BlacklistEntry.create({ tipId: String(tipId) });
+  async addItem(tipId) {
+    const entry = await BlacklistEntry.create({ tipId: String(tipId) });
 
-  await resetCache();
-  return entry;
-}
+    await BlacklistLogic.resetCache();
+    return entry;
+  },
 
-async function flagTip(tipId, author) {
-  let entry = await BlacklistEntry.findOne({ where: { tipId }, raw: true });
-  if (!entry) {
-    entry = await BlacklistEntry.create({ tipId, flagger: author, status: BLACKLIST_STATUS.FLAGGED });
-    // Kill stats cache
-    await resetCache();
-  }
-  return entry;
-}
+  async flagTip(tipId, author) {
+    let entry = await BlacklistEntry.findOne({ where: { tipId }, raw: true });
+    if (!entry) {
+      entry = await BlacklistEntry.create({ tipId, flagger: author, status: BLACKLIST_STATUS.FLAGGED });
+      // Kill stats cache
+      await BlacklistLogic.resetCache();
+    }
+    return entry;
+  },
 
-async function updateItem(tipId, status) {
-  await BlacklistEntry.update({ status }, { where: { tipId } });
-  await resetCache();
-}
+  async updateItem(tipId, status) {
+    await BlacklistEntry.update({ status }, { where: { tipId } });
+    await BlacklistLogic.resetCache();
+  },
 
-async function removeItem(tipId) {
-  const result = await BlacklistEntry.destroy({
-    where: {
-      tipId,
-    },
-  });
-  await resetCache();
-  return result;
-}
+  async removeItem(tipId) {
+    const result = await BlacklistEntry.destroy({
+      where: {
+        tipId,
+      },
+    });
+    await BlacklistLogic.resetCache();
+    return result;
+  },
 
-async function getBlacklistedIds() {
-  const blacklist = await BlacklistEntry.findAll({ raw: true, where: { status: BLACKLIST_STATUS.HIDDEN } });
-  return blacklist.map(b => b.tipId);
-}
-
-module.exports = {
-  augmentAllItems,
-  addItem,
-  flagTip,
-  updateItem,
-  removeItem,
-  getBlacklistedIds,
+  async getBlacklistedIds() {
+    const blacklist = await BlacklistEntry.findAll({ raw: true, where: { status: BLACKLIST_STATUS.HIDDEN } });
+    return blacklist.map(b => b.tipId);
+  },
 };
+
+module.exports = BlacklistLogic;

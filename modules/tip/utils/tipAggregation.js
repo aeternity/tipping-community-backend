4dc20c871dd223d645d1fe0ad1bfd741d1a615dd
Philipp Piwo <github@piwo.space>
2021-02-24 16:05:56 +0100

    feat(tipdb): comments relation and commentCount

--- modules/tip/utils/tipAggregation.js
+++ modules/tip/utils/tipAggregation.js
@@ -1,7 +1,8 @@
 const { Tip } = require('../../../models');
 
 const TIP_AGGREGATION = {
-  TOTAL_URL_AMOUNT: [Tip.sequelize.literal('(SELECT SUM("Tips"."amount") FROM "Tips" WHERE "Tips"."url" = "Tips"."url")'), 'totalUrlAmount'],
+  TOTAL_URL_AMOUNT: [Tip.sequelize.literal('(SELECT SUM("Tips"."amount") FROM "Tips" WHERE "Tips"."url" = "Tip"."url")'), 'totalUrlAmount'],
+  COUNT_COMMENTS: [Tip.sequelize.literal('(SELECT COUNT("Comments"."id") FROM "Comments" WHERE "Comments"."tipId" = "Tip"."id")'), 'commentCount'],
 };
 
 module.exports = {

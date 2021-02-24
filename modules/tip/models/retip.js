54143e7c5d6294bc903eb94e88da2a6c1bbef02f
Philipp Piwo <github@piwo.space>
2021-02-24 12:19:39 +0100

    feat(tipdb): fix model relations and join

--- modules/tip/models/retip.js
+++ modules/tip/models/retip.js
@@ -1,21 +1,15 @@
-const Tip = require('./tip');
-
-module.exports = (sequelize, DataTypes) => {
-  const Retip = sequelize.define('Retip', {
-    // attributes
-    id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      primaryKey: true,
-    },
-    unclaimed: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-    },
-    sender: { type: DataTypes.STRING },
-  }, {
-    timestamps: true,
-  });
-  Retip.belongsTo(Tip(sequelize, DataTypes), { foreignKey: 'tipId' });
-  return Retip;
-};
+module.exports = (sequelize, DataTypes) => sequelize.define('Retip', {
+  // attributes
+  id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+  },
+  unclaimed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+  sender: {type: DataTypes.STRING},
+}, {
+  timestamps: true,
+});

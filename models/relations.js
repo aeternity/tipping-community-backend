1dce41f41012c194dd570cde458392352f21252b
Philipp Piwo <github@piwo.space>
2021-02-24 16:05:56 +0100

    feat(tipdb): comments relation and commentCount

--- models/relations.js
+++ models/relations.js
@@ -2,4 +2,7 @@ module.exports = (db) => {
   db.Retip.belongsTo(db.Tip, { foreignKey: 'tipId' });
   db.Tip.hasMany(db.Retip, { foreignKey: 'tipId' });
 
+  db.Comment.belongsTo(db.Tip, { foreignKey: 'tipId' });
+  db.Tip.hasMany(db.Comment, { foreignKey: 'tipId' });
+
 }

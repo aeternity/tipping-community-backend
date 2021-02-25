bd32418a4958b030dca155931567b33a0d2b1838
Philipp Piwo <github@piwo.space>
2021-02-25 19:32:47 +0100

    feat(tipdb): linkpreview relation

---
+++ migrations/20210225-13-link-preview-reference.js
@@ -0,0 +1,101 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * changeColumn "url" on table "Tips"
+ * changeColumn "url" on table "Tips"
+ *
+ **/
+
+var info = {
+    "revision": 12,
+    "name": "noname",
+    "created": "2021-02-25T18:30:57.619Z",
+    "comment": ""
+};
+
+var migrationCommands = function(transaction) {
+    return [{
+            fn: "changeColumn",
+            params: [
+                "Tips",
+                "url",
+                {
+                    "type": Sequelize.TEXT,
+                    "onUpdate": "NO ACTION",
+                    "onDelete": "NO ACTION",
+                    "references": {
+                        "model": "LinkPreviews",
+                        "key": "requestUrl"
+                    },
+                    "field": "url",
+                    "allowNull": true
+                },
+                {
+                    transaction: transaction
+                }
+            ]
+        }
+    ];
+};
+var rollbackCommands = function(transaction) {
+    return [{
+            fn: "changeColumn",
+            params: [
+                "Tips",
+                "url",
+                {
+                    "type": Sequelize.TEXT,
+                    "field": "url",
+                    "allowNull": true
+                },
+                {
+                    transaction: transaction
+                }
+            ]
+        }
+    ];
+};
+
+module.exports = {
+    pos: 0,
+    useTransaction: true,
+    execute: function(queryInterface, Sequelize, _commands)
+    {
+        var index = this.pos;
+        function run(transaction) {
+            const commands = _commands(transaction);
+            return new Promise(function(resolve, reject) {
+                function next() {
+                    if (index < commands.length)
+                    {
+                        let command = commands[index];
+                        console.log("[#"+index+"] execute: " + command.fn);
+                        index++;
+                        queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                    }
+                    else
+                        resolve();
+                }
+                next();
+            });
+        }
+        if (this.useTransaction) {
+            return queryInterface.sequelize.transaction(run);
+        } else {
+            return run(null);
+        }
+    },
+    up: function(queryInterface, Sequelize)
+    {
+        return this.execute(queryInterface, Sequelize, migrationCommands);
+    },
+    down: function(queryInterface, Sequelize)
+    {
+        return this.execute(queryInterface, Sequelize, rollbackCommands);
+    },
+    info: info
+};

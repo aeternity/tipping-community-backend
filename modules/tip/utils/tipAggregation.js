165b357ddcdef4926ecbbfeb4e9f97249a5f58b8
Philipp Piwo <github@piwo.space>
2021-02-25 09:26:44 +0100

    feat(tipdb): total amount aggregation

--- modules/tip/utils/tipAggregation.js
+++ modules/tip/utils/tipAggregation.js
@@ -1,10 +1,7 @@
 const { Tip } = require('../../../models');
 
-const TIP_AGGREGATION = {
+module.exports = {
   TOTAL_URL_AMOUNT: [Tip.sequelize.literal('(SELECT SUM("Tips"."amount") FROM "Tips" WHERE "Tips"."url" = "Tip"."url")'), 'totalUrlAmount'],
   COUNT_COMMENTS: [Tip.sequelize.literal('(SELECT COUNT("Comments"."id") FROM "Comments" WHERE "Comments"."tipId" = "Tip"."id")'), 'commentCount'],
-};
-
-module.exports = {
-  TIP_AGGREGATION,
+  TOTAL_AMOUNT: [Tip.sequelize.literal('(SELECT SUM("Retips"."amount") + "Tip"."amount" FROM "Retips" WHERE "Retips"."tipId" = "Tip"."id")'), 'totalAmount'],
 };

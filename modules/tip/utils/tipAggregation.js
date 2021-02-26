6198bcc04115fdc1fa05a553e20e15c90155abf9
Philipp Piwo <github@piwo.space>
2021-02-26 09:42:49 +0100

    feat(tipdb): aggregate token total amount

--- modules/tip/utils/tipAggregation.js
+++ modules/tip/utils/tipAggregation.js
@@ -4,4 +4,5 @@ module.exports = {
   TOTAL_URL_AMOUNT: [Tip.sequelize.literal('(SELECT SUM("Tips"."amount") FROM "Tips" WHERE "Tips"."url" = "Tip"."url")'), 'totalUrlAmount'],
   COUNT_COMMENTS: [Tip.sequelize.literal('(SELECT COUNT("Comments"."id") FROM "Comments" WHERE "Comments"."tipId" = "Tip"."id")'), 'commentCount'],
   TOTAL_AMOUNT: [Tip.sequelize.literal('(SELECT SUM("Retips"."amount") + "Tip"."amount" FROM "Retips" WHERE "Retips"."tipId" = "Tip"."id")'), 'totalAmount'],
+  TOKEN_TOTAL_AMOUNT: [Tip.sequelize.literal('ARRAY(SELECT JSON_BUILD_OBJECT(COALESCE("Retips"."token", "Tip"."token"), SUM("Retips"."tokenAmount") + "Tip"."tokenAmount") FROM "Retips" WHERE "Retips"."tipId" = "Tip"."id" AND COALESCE("Retips"."token", "Tip"."token") IS NOT NULL GROUP BY COALESCE("Retips"."token", "Tip"."token"))'), 'tokenTotalAmount'],
 };

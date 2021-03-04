9a51c6b7a51bae0343f147ed3affc939dc56fbbd
Philipp Piwo <github@piwo.space>
2021-03-04 15:17:35 +0100

    feat(tipdb): aggregation with materialized view

--- modules/tip/utils/tipAggregation.js
+++ modules/tip/utils/tipAggregation.js
@@ -1,11 +1,7 @@
 const { Tip } = require('../../../models');
 
 module.exports = {
-  TOTAL_URL_AMOUNT: [Tip.sequelize.literal('(SELECT SUM("Tips"."amount") FROM "Tips" WHERE "Tips"."url" = "Tip"."url")'), 'totalUrlAmount'],
+  AGGREGATION_VIEW: [Tip.sequelize.literal('(SELECT ROW_TO_JSON("TipsAggregation".*) FROM TipsAggregation AS "TipsAggregation" WHERE "TipsAggregation"."id" = "Tip"."id")'), 'Aggregation'],
   COUNT_COMMENTS: [Tip.sequelize.literal('(SELECT COUNT("Comments"."id") FROM "Comments" WHERE "Comments"."tipId" = "Tip"."id")'), 'commentCount'],
-  TOTAL_AMOUNT: [Tip.sequelize.literal('(SELECT total_amount("Tip"."id"))'), 'totalAmount'],
-  TOTAL_UNCLAIMED_AMOUNT: [Tip.sequelize.literal('(SELECT total_unclaimed_amount("Tip"."id"))'), 'totalUnclaimedAmount'],
-  TOKEN_TOTAL_AMOUNT: [Tip.sequelize.literal('ARRAY(SELECT JSON_BUILD_OBJECT(COALESCE("Retips"."token", "Tip"."token"), SUM("Retips"."tokenAmount") + "Tip"."tokenAmount") FROM "Retips" WHERE "Retips"."tipId" = "Tip"."id" AND COALESCE("Retips"."token", "Tip"."token") IS NOT NULL GROUP BY COALESCE("Retips"."token", "Tip"."token"))'), 'tokenTotalAmount'],
-  TOKEN_TOTAL_UNCLAIMED_AMOUNT: [Tip.sequelize.literal('ARRAY(SELECT JSON_BUILD_OBJECT(COALESCE("Retips"."token", "Tip"."token"), SUM(CASE WHEN unclaimed("Retips"."claimGen", "Tip"."url", "Tip"."contractId") THEN "Retips"."tokenAmount" ELSE 0 END) + (CASE WHEN unclaimed("Tip"."claimGen", "Tip"."url", "Tip"."contractId") THEN "Tip"."tokenAmount" ELSE 0 END)) FROM "Retips" WHERE "Retips"."tipId" = "Tip"."id" AND COALESCE("Retips"."token", "Tip"."token") IS NOT NULL GROUP BY COALESCE("Retips"."token", "Tip"."token"))'), 'tokenTotalUnclaimedAmount'],
-  SCORE: [Tip.sequelize.literal('(SELECT dateScore * 1.5 + amountScore * 1 FROM (SELECT (GREATEST(0, 1 + LOG10(1 - EXTRACT(EPOCH FROM (NOW() - (GREATEST(NOW() - INTERVAL \'14 DAYS\', "Tip".timestamp)))) / EXTRACT(EPOCH FROM INTERVAL \'14 DAYS 1 SECOND\')))) AS dateScore, (total_amount("Tip"."id") / (SELECT MAX(total_amount("Tips"."id")) FROM "Tips")) AS amountScore) AS score)'), 'score'],
+  SCORE: [Tip.sequelize.literal('(SELECT dateScore * 1.5 + amountScore * 1 FROM (SELECT (GREATEST(0, 1 + LOG10(1 - EXTRACT(EPOCH FROM (NOW() - (GREATEST(NOW() - INTERVAL \'14 DAYS\', "Tip".timestamp)))) / EXTRACT(EPOCH FROM INTERVAL \'14 DAYS 1 SECOND\')))) AS dateScore, ((SELECT "totalamount"::NUMERIC FROM TipsAggregation WHERE "id" = "Tip"."id") / (SELECT MAX("totalamount"::NUMERIC) FROM TipsAggregation)) AS amountScore) AS score)'), 'score'],
 };

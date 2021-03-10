7320b44702ca6f4a31982f4ca0d9ffc97047679d
Philipp Piwo <github@piwo.space>
2021-03-10 14:24:29 +0100

    feat(tipdb): sample full text search

--- modules/tip/utils/tipFilter.js
+++ modules/tip/utils/tipFilter.js
@@ -1,6 +1,18 @@
 const { Tip } = require('../../../models');
-const Sequelize = require('sequelize');
+const { Op } = require('sequelize');
 
 module.exports = {
-  FILTER_BLACKLIST: {[Sequelize.Op.notIn]:Tip.sequelize.literal('(SELECT "BlacklistEntries"."tipId" FROM "BlacklistEntries" WHERE "BlacklistEntries"."tipId" = "Tip"."id")')},
+  FILTER_BLACKLIST: {[Op.notIn]: Tip.sequelize.literal('(SELECT "BlacklistEntries"."tipId" FROM "BlacklistEntries" WHERE "BlacklistEntries"."tipId" = "Tip"."id")')},
+  FILTER_SIMILARITY_SUM: (search) =>
+    Tip.sequelize.fn(
+      'sum_array',
+      Tip.sequelize.fn('ARRAY_PREPEND',
+        Tip.sequelize.fn('SIMILARITY', Tip.sequelize.col('Tip.title'), search),
+        Tip.sequelize.fn('ARRAY_PREPEND',
+          Tip.sequelize.fn('SIMILARITY', Tip.sequelize.col('Tip.sender'), search),
+          Tip.sequelize.fn('ARRAY_PREPEND',
+            Tip.sequelize.fn('SIMILARITY', Tip.sequelize.col('Tip.url'), search),
+            Tip.sequelize.literal('ARRAY[0::REAL]')
+          )))
+    ),
 };

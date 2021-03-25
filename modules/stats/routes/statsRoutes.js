4f38692189ac94bc6f0739a4ab51ea785dfe93a3
Philipp Piwo <github@piwo.space>
2021-03-25 14:06:05 +0100

    feat(tipdb): expose view sender stats

--- modules/stats/routes/statsRoutes.js
+++ modules/stats/routes/statsRoutes.js
@@ -23,4 +23,32 @@ router.get('/', async (req, res) => {
   return stats ? res.send(stats) : res.sendStatus(404);
 });
 
+/**
+ * @swagger
+ * /stats/sender:
+ *   get:
+ *     tags:
+ *       - stats
+ *     summary: Returns aggregated stats for a single address
+ *     parameters:
+ *       - in: query
+ *         name: address
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: users address to only query stats for this specific user
+ *     responses:
+ *       200:
+ *         description: Returns aggregated stats for a single address
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ */
+router.get('/sender', async (req, res) => {
+  const stats = await StatsLogic.fetchUserStats(req.query.address);
+  return stats ? res.send(stats) : res.sendStatus(404);
+
+});
+
 module.exports = router;

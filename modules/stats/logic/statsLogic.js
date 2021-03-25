4878befd4a628666ec781c82a2d4a27bc2459179
Philipp Piwo <github@piwo.space>
2021-03-25 14:06:05 +0100

    feat(tipdb): expose view sender stats

--- modules/stats/logic/statsLogic.js
+++ modules/stats/logic/statsLogic.js
@@ -5,4 +5,12 @@ module.exports = class StatsLogic {
     const [results] = await sequelize.query("SELECT ROW_TO_JSON(stats.*) as stats FROM stats");
     return results.length ? results[0].stats : null;
   }
+
+  static async fetchUserStats(address) {
+    const [results] = await sequelize.query('SELECT ROW_TO_JSON(senderstats.*) as senderstats FROM senderstats WHERE sender = ?',
+      { replacements: [address], type: sequelize.QueryTypes.SELECT }
+    )
+
+    return results ? results.senderstats : null;
+  }
 }

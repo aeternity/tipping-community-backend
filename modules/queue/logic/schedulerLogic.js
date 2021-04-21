d77810c092a84db46b9db58eb9113c92617da935
Philipp Piwo <github@piwo.space>
2021-04-21 16:51:18 +0200

    feat(tipdb): add scheduler logic for queue

---
+++ modules/queue/logic/schedulerLogic.js
@@ -0,0 +1,20 @@
+const queueLogic = require('./queueLogic');
+const { MESSAGES, MESSAGE_QUEUES } = require('../constants/queue');
+
+const msInMin = 60 * 1000;
+
+const scheduledEvents = [
+  { message: MESSAGES.SCHEDULED_EVENTS.COMMANDS.CACHE_KEEPHOT, interval: process.env.KEEP_HOT_INTERVAL || 5 * msInMin, onceAtStartup: true },
+  { message: MESSAGES.SCHEDULED_EVENTS.COMMANDS.UPDATE_CHAIN_NAMES, interval: 10 * msInMin, onceAtStartup: true },
+];
+
+const SchedulerLogic = {
+  init() {
+    setTimeout(() => scheduledEvents.forEach(async ({ message, interval, onceAtStartup }) => {
+      if (onceAtStartup) await queueLogic.sendMessage(MESSAGE_QUEUES.SCHEDULED_EVENTS, message);
+      setInterval(() => queueLogic.sendMessage(MESSAGE_QUEUES.SCHEDULED_EVENTS, message), interval);
+    }), 1000);
+  },
+};
+
+module.exports = SchedulerLogic;

7ba0f8b05ce6b13e558f97a12c4fde501fc2659b
Philipp Piwo <github@piwo.space>
2021-04-21 10:17:39 +0200

    feat(tipdb): fixes after rebase

--- modules/aeternity/logic/chainListenerLogic.js
+++ modules/aeternity/logic/chainListenerLogic.js
@@ -12,7 +12,7 @@ const wsclient = new WebSocketClient();
 // Check if all envs are defined
 if (!process.env.WEBSOCKET_URL) throw new Error('WEBSOCKET_URL is not set');
 
-const handleContractEvent = async  (event, tx) => {
+const handleContractEvent = async (event, tx) => {
   await queueLogic.sendMessage(MESSAGE_QUEUES.BLOCKCHAIN, MESSAGES.BLOCKCHAIN.EVENTS.EVENT_RECEIVED, { event, tx });
 };
 

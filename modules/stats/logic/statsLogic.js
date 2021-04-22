1e57b56749098d71cc052454e5bd0888ef12161d
Philipp Piwo <github@piwo.space>
2021-04-22 10:29:07 +0200

    feat(tipdb): added url stats to stats endpoint

--- modules/stats/logic/statsLogic.js
+++ modules/stats/logic/statsLogic.js
@@ -1,18 +1,19 @@
 const { sequelize, Comment } = require('../../../models');
 const CacheLogic = require('../../cache/logic/cacheLogic');
-const { URL_STATS, SENDER_STATS, GLOBAL_STATS } = require('../utils/statsAggregation');
+const { SUM_URL_STATS_FOR_URLS, SENDER_STATS, GLOBAL_STATS, URL_STATS } = require('../utils/statsAggregation');
 
 module.exports = class StatsLogic {
   static async fetchStats() {
-    const [results] = await sequelize.query(GLOBAL_STATS);
-    return results.length ? results[0].stats : null;
+    const [stats] = await sequelize.query(GLOBAL_STATS);
+    const [urlStats] = await sequelize.query(URL_STATS);
+    return stats.length ? { ...stats[0], urlStats: urlStats } : null;
   }
 
   static async fetchUserStats(address) {
     const claimedUrls = await CacheLogic.getOracleClaimedUrls(address);
 
     const [results] = await sequelize.query(SENDER_STATS, { replacements: [address], type: sequelize.QueryTypes.SELECT });
-    const [urlStats] = await sequelize.query(URL_STATS, { replacements: [claimedUrls], type: sequelize.QueryTypes.SELECT });
+    const [urlStats] = await sequelize.query(SUM_URL_STATS_FOR_URLS, { replacements: [claimedUrls], type: sequelize.QueryTypes.SELECT });
     const commentCount = await Comment.count({ where: { author: address } });
 
     return {

428d41043dfb913016cd0ccda69645faa2b071ca
Philipp Piwo <github@piwo.space>
2021-04-22 16:52:12 +0200

    feat(tipdb): fix caseing

--- modules/stats/logic/statsLogic.js
+++ modules/stats/logic/statsLogic.js
@@ -1,12 +1,14 @@
 const { sequelize, Comment } = require('../../../models');
 const CacheLogic = require('../../cache/logic/cacheLogic');
-const { SUM_URL_STATS_FOR_URLS, SENDER_STATS, GLOBAL_STATS, URL_STATS } = require('../utils/statsAggregation');
+const {
+  SUM_URL_STATS_FOR_URLS, SENDER_STATS, GLOBAL_STATS, URL_STATS,
+} = require('../utils/statsAggregation');
 
 module.exports = class StatsLogic {
   static async fetchStats() {
     const [stats] = await sequelize.query(GLOBAL_STATS);
     const [urlStats] = await sequelize.query(URL_STATS);
-    return stats.length ? { ...stats[0], urlStats: urlStats } : null;
+    return stats.length ? { ...stats[0], urlStats } : null;
   }
 
   static async fetchUserStats(address) {

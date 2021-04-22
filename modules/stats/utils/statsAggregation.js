c4c8b29a242916055c9ecbe5a6d46c71adca3580
Philipp Piwo <github@piwo.space>
2021-04-22 10:29:07 +0200

    feat(tipdb): added url stats to stats endpoint

--- modules/stats/utils/statsAggregation.js
+++ modules/stats/utils/statsAggregation.js
@@ -1,7 +1,8 @@
 /* eslint-disable max-len */
 
 module.exports = {
-  GLOBAL_STATS: 'SELECT ROW_TO_JSON(stats.*) as stats FROM stats',
-  URL_STATS: 'SELECT SUM(urlstats.totaltipslength) AS totaltipslength, SUM(urlstats.totalamount::NUMERIC)::VARCHAR AS totalamount FROM urlstats WHERE url IN (?);',
+  GLOBAL_STATS: 'SELECT stats.* as stats FROM stats',
+  URL_STATS: 'SELECT urlstats.* as urlstats FROM urlstats',
+  SUM_URL_STATS_FOR_URLS: 'SELECT SUM(urlstats.totaltipslength) AS totaltipslength, SUM(urlstats.totalamount::NUMERIC)::VARCHAR AS totalamount FROM urlstats WHERE url IN (?);',
   SENDER_STATS: 'SELECT ROW_TO_JSON(senderstats.*) as senderstats FROM senderstats WHERE sender = ?;',
 };

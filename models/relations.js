3e0adc5f322059def0cdbe644159e7250e6307f2
Philipp Piwo <github@piwo.space>
2021-04-22 16:52:12 +0200

    feat(tipdb): fix caseing

--- models/relations.js
+++ models/relations.js
@@ -1,32 +1,33 @@
 module.exports = (db, sequelize, Op) => {
-  db.Retip.belongsTo(db.Tip, { foreignKey: 'tipId' });
-  db.Tip.hasMany(db.Retip, { foreignKey: 'tipId' });
+  db.Retip.belongsTo(db.Tip, { as: 'tip', foreignKey: 'tipId' });
+  db.Tip.hasMany(db.Retip, { as: 'retips', foreignKey: 'tipId' });
 
   db.Comment.belongsTo(db.Tip, { foreignKey: 'tipId' });
   db.Tip.hasMany(db.Comment, { foreignKey: 'tipId' });
 
   db.LinkPreview.hasOne(db.Tip, {
-    optional: true, sourceKey: 'requestUrl', foreignKey: 'url', onDelete: 'NO ACTION', onUpdate: 'NO ACTION',
+    as: 'tip', optional: true, sourceKey: 'requestUrl', foreignKey: 'url', onDelete: 'NO ACTION', onUpdate: 'NO ACTION',
   });
   db.Tip.belongsTo(db.LinkPreview, {
-    optional: true, targetKey: 'requestUrl', foreignKey: 'url', onDelete: 'NO ACTION', onUpdate: 'NO ACTION',
+    as: 'linkPreview', optional: true, targetKey: 'requestUrl', foreignKey: 'url', onDelete: 'NO ACTION', onUpdate: 'NO ACTION',
   });
 
   db.BlacklistEntry.belongsTo(db.Tip, { foreignKey: 'tipId', onDelete: 'NO ACTION', onUpdate: 'NO ACTION' });
   db.Tip.hasMany(db.BlacklistEntry, { foreignKey: 'tipId', onDelete: 'NO ACTION', onUpdate: 'NO ACTION' });
 
   db.Tip.hasOne(db.Claim, {
+    as: 'claim',
     foreignKey: 'url',
     sourceKey: 'url',
     scope: {
       [Op.and]: sequelize.where(sequelize.col('Tip.contractId'),
         Op.eq,
-        sequelize.col('Claim.contractId')),
+        sequelize.col('claim.contractId')),
     },
     constraints: false,
   });
 
   db.Tip.hasOne(db.ChainName, {
-    sourceKey: 'sender', foreignKey: 'publicKey', constraints: false, optional: true,
+    as: 'chainName', sourceKey: 'sender', foreignKey: 'publicKey', constraints: false, optional: true,
   });
 };

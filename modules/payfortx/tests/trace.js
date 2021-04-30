dc084421bef0151f43c1856359d2efa380d8f6c3
Keno Dressel <hire@keno.digital>
2021-04-30 14:17:40 +0200

    tests(trace): updates tests after refactoring

--- modules/payfortx/tests/trace.js
+++ modules/payfortx/tests/trace.js
@@ -7,11 +7,11 @@ const sinon = require('sinon');
 const BigNumber = require('bignumber.js');
 const server = require('../../../server');
 const ae = require('../../aeternity/logic/aeternity');
-const cache = require('../../cache/utils/cache');
-const { Trace: TraceModel } = require('../../../models');
+const { Trace } = require('../../../models');
 const { publicKey } = require('../../../utils/testingUtil');
 const CacheLogic = require('../../cache/logic/cacheLogic');
 const EventLogic = require('../../event/logic/eventLogic');
+const { fakeTipsAndUpdateDB } = require('../../../utils/testingUtil');
 
 chai.should();
 chai.use(chaiHttp);
@@ -19,28 +19,32 @@ chai.use(chaiHttp);
 describe('Trace', () => {
   before(async function () { // Before each test we empty the database
     this.timeout(10000);
-    await TraceModel.destroy({
-      where: {},
-      truncate: true,
-    });
+    await Trace.truncate();
 
     await ae.init();
-    await cache.init(ae);
   });
 
+  const seedDB = fakeTipsAndUpdateDB([Trace]);
+
   describe('TraceLogic API Backend', () => {
-    it('it should GET all traces for tipID 0', done => {
-      const stub = sinon.stub(CacheLogic, 'getTips').callsFake(async () => [{
-        id: '0',
-        url: 'https://aeternity.com',
-      }]);
+    it('it should GET zero traces for non existing tip id', done => {
       chai.request(server).get('/tracing/backend?id=0').end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('array');
-        res.body.length.should.be.eql(0);
-        stub.restore();
+        res.should.have.status(404);
         done();
       });
+    });
+
+    it('it should GET zero traces for an existing tip with no traces', async () => {
+      await seedDB({
+        tips: [{
+          id: '1_v1',
+        }],
+      });
+
+      const res = await chai.request(server).get('/tracing/backend?id=1_v1');
+      res.should.have.status(200);
+      res.body.should.be.a('array');
+      res.body.length.should.be.eql(0);
     });
 
     it('malformed request without url should not leave a trace', done => {
@@ -50,7 +54,7 @@ describe('Trace', () => {
         })
         .end((err, res) => {
           res.should.have.status(400);
-          TraceModel.findAll({ raw: true }).then(results => {
+          Trace.findAll({ raw: true }).then(results => {
             results.should.have.length(0);
             done();
           });
@@ -66,7 +70,7 @@ describe('Trace', () => {
         })
         .end((err, res) => {
           res.should.have.status(400);
-          TraceModel.findAll({ raw: true }).then(results => {
+          Trace.findAll({ raw: true }).then(results => {
             results.should.have.length(0);
             done();
           });
@@ -89,14 +93,17 @@ describe('Trace', () => {
         });
     });
 
-    it('it should GET all traces for a proper tip', function (done) {
-      this.timeout(20000);
-      chai.request(server).get('/tracing/backend?id=462_v1').end((err, res) => { // 462_v1 == example.com
-        res.should.have.status(200);
-        res.body.should.be.a('array');
-        res.body.length.should.be.eql(1);
-        done();
-      });
+    it('it should GET all traces for a proper tip', async () => {
+      await seedDB({
+        tips: [{
+          id: '462_v1',
+          url: 'example.com',
+        }],
+      }, false);
+      const res = await chai.request(server).get('/tracing/backend?id=462_v1'); // 462_v1 == example.com
+      res.should.have.status(200);
+      res.body.should.be.a('array');
+      res.body.length.should.be.eql(1);
     });
 
     it('it should GET all blockchain traces for a proper tip', function (done) {

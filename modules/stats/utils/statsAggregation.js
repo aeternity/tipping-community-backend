f5727296d4d9348e854cead69d522b405d65fe3d
Philipp Piwo <github@piwo.space>
2021-04-30 18:15:41 +0200

    feat(tipdb): fixed tracing

--- modules/stats/utils/statsAggregation.js
+++ modules/stats/utils/statsAggregation.js
@@ -3,6 +3,7 @@
 module.exports = {
   GLOBAL_STATS: 'SELECT stats.* as "stats" FROM stats',
   URL_STATS: 'SELECT urlstats.* as "urlStats" FROM urlstats',
+  URL_STATS_BY_URL: 'SELECT urlstats.* as "urlStats" FROM urlstats WHERE url = ?;',
   SUM_URL_STATS_FOR_URLS: 'SELECT SUM(urlstats."totalTipsLength") AS "totalTipsLength", SUM(urlstats."totalAmount"::NUMERIC)::VARCHAR AS "totalAmount" FROM urlstats WHERE url IN (?);',
   SENDER_STATS: 'SELECT ROW_TO_JSON(senderstats.*) as senderstats FROM senderstats WHERE sender = ?;',
 };

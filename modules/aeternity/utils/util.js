e75fac957451334f7a13b0aa82827d35adede83e
Keno Dressel <hire@keno.digital>
2021-05-05 12:05:18 +0200

    refactor(util): move asyncMap to server.js

--- modules/aeternity/utils/util.js
+++ modules/aeternity/utils/util.js
@@ -1,8 +1,3 @@
-const BigNumber = require('bignumber.js');
-
-const atomsToAe = atoms => (new BigNumber(atoms)).dividedBy(new BigNumber(1000000000000000000));
-const aeToAtoms = ae => (new BigNumber(ae)).times(new BigNumber(1000000000000000000));
-
 Array.prototype.asyncMap = async function (asyncF) {
   return this.reduce(async (promiseAcc, cur) => {
     const acc = await promiseAcc;
@@ -13,12 +8,4 @@ Array.prototype.asyncMap = async function (asyncF) {
     if (res) acc.push(res);
     return acc;
   }, Promise.resolve([]));
-};
-
-const groupBy = (xs, key) => xs.reduce((acc, x) => ({ ...acc, [x[key]]: (acc[x[key]] || []).concat(x) }), {});
-
-module.exports = {
-  atomsToAe,
-  aeToAtoms,
-  groupBy,
 };

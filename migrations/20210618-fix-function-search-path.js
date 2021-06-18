d7fbb57723e0a8c71ac6e0529368378bfe795eca
Philipp Piwo <github@piwo.space>
2021-06-18 11:40:25 +0200

    fix(db): fix functions search path for backups

--- migrations/20210618-fix-function-search-path.js
+++ migrations/20210618-fix-function-search-path.js
@@ -16,55 +16,19 @@ var info = {
     "comment": ""
 };
 
-var migrationCommands = function(transaction) {
-    return [];
-};
-var rollbackCommands = function(transaction) {
-    return [];
-};
-
 module.exports = {
     pos: 0,
     useTransaction: true,
-    execute: function(queryInterface, Sequelize, _commands)
-    {
-        var index = this.pos;
-        function run(transaction) {
-            const commands = _commands(transaction);
-            return new Promise(function(resolve, reject) {
-                function next() {
-                    if (index < commands.length)
-                    {
-                        let command = commands[index];
-                        console.log("[#"+index+"] execute: " + command.fn);
-                        index++;
-                        queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
-                    }
-                    else
-                        resolve();
-                }
-                next();
-            });
-        }
-        if (this.useTransaction) {
-            return queryInterface.sequelize.transaction(run);
-        } else {
-            return run(null);
-        }
-    },
-    up: async function(queryInterface, Sequelize)
-    {
-      await this.execute(queryInterface, Sequelize, migrationCommands);
 
+    up: async function(queryInterface)
+    {
       const transaction = await queryInterface.sequelize.transaction();
       await queryInterface.sequelize.query('ALTER FUNCTION unclaimed(numeric, text, varchar) SET search_path=public;', { transaction });
       await queryInterface.sequelize.query('ALTER FUNCTION unclaimed_amount(numeric, text, varchar, numeric) SET search_path=public;', { transaction });
       await queryInterface.sequelize.query('ALTER FUNCTION claimed_amount(numeric, text, varchar, numeric) SET search_path=public;', { transaction });
       return transaction.commit();
     },
-    down: async function(queryInterface, Sequelize)
-    {
-      return this.execute(queryInterface, Sequelize, rollbackCommands);
-    },
+  down: () => {},
+
     info: info
 };

218fdb93d6fca32f47b5437e43e4437b47128edc
Keno Dressel <hire@keno.digital>
2021-06-29 10:43:43 +0200

    refactor(health): class to module

--- modules/health/logic/healthLogic.js
+++ modules/health/logic/healthLogic.js
@@ -3,14 +3,14 @@ const cache = require('../../cache/utils/cache');
 const models = require('../../../models');
 const aeternity = require('../../aeternity/logic/aeternity');
 
-module.exports = class HealthLogic {
+const HealthLogic = {
   /**
    * Checks for database consistency
    * Its using the ORM to query all tables which errors
    * if fields are not according to the model
    * @returns {Promise<boolean>}
    */
-  static async checkDBHealth() {
+  async checkDBHealth() {
     try {
       await Promise.all(Object.keys(models)
         .filter(key => key.toLowerCase() !== 'sequelize')
@@ -19,36 +19,36 @@ module.exports = class HealthLogic {
     } catch (e) {
       return false;
     }
-  }
+  },
 
-  static async checkIPFSHealth() {
+  async checkIPFSHealth() {
     try {
       await ipfs.getCoreVitals();
       return true;
     } catch (e) {
       return false;
     }
-  }
+  },
 
-  static async checkRedisHealth() {
+  async checkRedisHealth() {
     try {
       await cache.getOrSet(['redisTest'], async () => 'done');
       return true;
     } catch (e) {
       return false;
     }
-  }
+  },
 
-  static async checkAEClient() {
+  async checkAEClient() {
     try {
       const balance = await aeternity.getBalance();
       return parseInt(balance, 10) > 0;
     } catch (e) {
       return false;
     }
-  }
+  },
 
-  static async answerHealthRequest(req, res) {
+  async answerHealthRequest(req, res) {
     const dbHealth = await HealthLogic.checkDBHealth();
     const ipfsHealth = await HealthLogic.checkIPFSHealth();
     const redisHealth = await HealthLogic.checkRedisHealth();
@@ -57,5 +57,7 @@ module.exports = class HealthLogic {
     res.status(allHealthy ? 200 : 500).send({
       dbHealth, ipfsHealth, redisHealth, aeHealth, allHealthy,
     });
-  }
+  },
 };
+
+module.exports = HealthLogic;

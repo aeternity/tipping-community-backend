1319dd60d6a1c726028e405b7ff0d7966bc1449f
Keno Dressel <hire@keno.digital>
2021-07-01 11:12:34 +0200

    refactor(pin): moves request handling to routes

--- modules/pin/logic/pinLogic.js
+++ modules/pin/logic/pinLogic.js
@@ -1,43 +1,31 @@
 const { Pin } = require('../../../models');
 const { PINNED_CONTENT_TYPES } = require('../constants/contentTypes');
 const TipLogic = require('../../tip/logic/tipLogic');
-const logger = require('../../../utils/logger')(module);
 
 const PinLogic = {
-  async addItem(req, res) {
-    try {
-      const {
-        entryId, type, signature, challenge,
-      } = req.body;
-      const { author } = req.params;
-      if (!entryId || !type || !author || !signature || !challenge) res.status(400).send('Missing required field');
-      if (!PINNED_CONTENT_TYPES[type]) return res.status(400).send(`Send type is invalid ${type}`);
-      const entry = await Pin.create({
-        entryId, type, author, signature, challenge,
-      });
-      return res.send(entry);
-    } catch (e) {
-      logger.error(e);
-      return res.status(500).send(e.message);
-    }
+  async addItem({
+    entryId, type, signature, challenge, author,
+  }) {
+    return Pin.create({
+      entryId, type, author, signature, challenge,
+    });
   },
 
-  async removeItem(req, res) {
-    const result = await Pin.destroy({
+  async removeItem(entryId, author, type) {
+    return Pin.destroy({
       where: {
-        entryId: req.body.entryId,
-        author: req.params.author,
-        type: req.body.type,
+        entryId,
+        author,
+        type,
       },
     });
-    return result === 1 ? res.sendStatus(200) : res.sendStatus(404);
   },
 
-  async getAllItemsPerUser(req, res) {
+  async getAllItemsPerUser(author) {
     const tips = await TipLogic.fetchAllLocalTips();
-    const pins = (await Pin.findAll({ where: { author: req.params.author }, raw: true }))
+    const pins = (await Pin.findAll({ where: { author }, raw: true }))
       .filter(pin => pin.type === PINNED_CONTENT_TYPES.TIP).map(pin => pin.entryId);
-    return res.send(tips.filter(({ id }) => pins.includes(String(id))));
+    return tips.filter(({ id }) => pins.includes(String(id)));
   },
 };
 

bf66ab12f104aac0f7701937f5b6f66a75899003
Philipp Piwo <github@piwo.space>
2021-07-21 12:45:39 +0200

    feat(stats): add stats endpoint for marketing

--- modules/stats/logic/statsLogic.js
+++ modules/stats/logic/statsLogic.js
@@ -1,7 +1,7 @@
 const { sequelize, Comment } = require('../../../models');
 const CacheLogic = require('../../cache/logic/cacheLogic');
 const {
-  SUM_URL_STATS_FOR_URLS, SENDER_STATS, GLOBAL_STATS, URL_STATS, URL_STATS_BY_URL,
+  SUM_URL_STATS_FOR_URLS, SENDER_STATS, GLOBAL_STATS, URL_STATS, URL_STATS_BY_URL, MARKETING_STATS,
 } = require('../utils/statsAggregation');
 
 const StatsLogic = {
@@ -33,6 +33,22 @@ const StatsLogic = {
       claimedUrlsLength: claimedUrls.length,
       urlStats,
       ...results ? results.senderstats : {},
+    };
+  },
+
+  async fetchMarketingStats() {
+    const getForDays = async days => {
+      const [daysStats] = await sequelize.query(MARKETING_STATS, {
+        replacements: [...Array(10).keys()].map(() => String(days)),
+        type: sequelize.QueryTypes.SELECT,
+      });
+      return daysStats;
+    };
+
+    return {
+      '1day': await getForDays(1),
+      '7days': await getForDays(7),
+      '30days': await getForDays(30),
     };
   },
 };

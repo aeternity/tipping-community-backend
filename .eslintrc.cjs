5f75fbd5b808465db5d54ecea5d6611d5f524948
Keno Dressel <hire@keno.digital>
2024-04-24 17:33:29 +0200

    chore: removes all lint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,14 +7,5 @@ module.exports = {
     sourceType: "module",
     ecmaVersion: 2020,
   },
-  rules: {
-    "no-use-before-define": "off",
-    "no-extend-native": "off",
-    "max-len": ["error", { code: 150 }],
-    "class-methods-use-this": "off",
-    "no-plusplus": "off",
-    "func-names": ["error", "never"],
-    "arrow-parens": ["error", "as-needed"],
-    "no-await-in-loop": "off",
-  },
+  extends: ["plugin:prettier/recommended"],
 };

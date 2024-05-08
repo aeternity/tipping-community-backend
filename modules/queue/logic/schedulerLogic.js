9f5cfe6138ea5e5ae9e914a26ae3a13dc4ab3300
Keno Dressel <hire@keno.digital>
2024-05-08 09:09:01 +0200

    fix: disables messages on startup to avoid doing everything at once

--- modules/queue/logic/schedulerLogic.js
+++ modules/queue/logic/schedulerLogic.js
@@ -4,11 +4,11 @@ const { MESSAGES, MESSAGE_QUEUES } = require('../constants/queue');
 const msInMin = 60 * 1000;
 
 const scheduledEvents = [
-  { message: MESSAGES.SCHEDULED_EVENTS.COMMANDS.CACHE_KEEPHOT, interval: process.env.KEEP_HOT_INTERVAL || 5 * msInMin, onceAtStartup: true },
-  { message: MESSAGES.SCHEDULED_EVENTS.COMMANDS.UPDATE_CHAIN_NAMES, interval: 10 * msInMin, onceAtStartup: true },
-  { message: MESSAGES.SCHEDULED_EVENTS.COMMANDS.UPDATE_TIPS_RETIPS_CLAIMS, interval: 10 * msInMin, onceAtStartup: true },
-  { message: MESSAGES.SCHEDULED_EVENTS.COMMANDS.UPDATE_EVENTS, interval: 10 * msInMin, onceAtStartup: true },
-  { message: MESSAGES.SCHEDULED_EVENTS.COMMANDS.UPDATE_LINKPREVIEWS, interval: 60 * msInMin, onceAtStartup: true },
+  { message: MESSAGES.SCHEDULED_EVENTS.COMMANDS.CACHE_KEEPHOT, interval: process.env.KEEP_HOT_INTERVAL || 5 * msInMin, onceAtStartup: false },
+  { message: MESSAGES.SCHEDULED_EVENTS.COMMANDS.UPDATE_CHAIN_NAMES, interval: 15 * msInMin, onceAtStartup: false }, // 10
+  { message: MESSAGES.SCHEDULED_EVENTS.COMMANDS.UPDATE_TIPS_RETIPS_CLAIMS, interval: 9 * msInMin, onceAtStartup: false }, // 10
+  { message: MESSAGES.SCHEDULED_EVENTS.COMMANDS.UPDATE_EVENTS, interval: 12 * msInMin, onceAtStartup: false }, // 10
+  { message: MESSAGES.SCHEDULED_EVENTS.COMMANDS.UPDATE_LINKPREVIEWS, interval: 60 * msInMin, onceAtStartup: false },
 ];
 
 const SchedulerLogic = {
